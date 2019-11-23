14811c4776ed535c7640200d3e3d17411d948569
John Cyzeski <jcyzeski@gmail.com>
2019-11-23 13:06:13 -0500

    updates on JS

---
+++ data.js
@@ -0,0 +1,14 @@
+let countries = [
+  {
+    name: 'United States',
+    flagUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/1920px-Flag_of_the_United_States.svg.png',
+  },
+  {
+    name: 'Nigeria',
+    flagUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Flag_of_Nigeria.svg/1920px-Flag_of_Nigeria.svg.png',
+  },
+  {
+    name: 'Colombia',
+    flagUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Flag_of_Colombia.svg/255px-Flag_of_Colombia.svg.png',
+  },
+];
--- scripts.js
+++ scripts.js
@@ -2,25 +2,56 @@ const nigeria = document.getElementById('nigeria');
 const us = document.getElementById('us');
 const colombia = document.getElementById('colombia');
 const container = document.getElementById('container');
+// const flagDiv = document.createElement('div').classList.add('flag-container');
+// const imageDiv = document.createElement('div').classList.add('image');
+// const textDiv = document.createElement('div').classList.add('text-div');
+// const p = document.createElement('p').classList.add('text');
 
+console.log(countries);
 
-container.addEventListener('mouseover', (e) => {
-  if (e.target === nigeria.children[0]) {
-    nigeria.children[1].children[0].textContent = 'Nigeria';
-  }  else if (e.target === us.children[0])  {
-    us.children[1].children[0].textContent = 'United States';
-  }  else if (e.target === colombia.children[0])  {
-    colombia.children[1].children[0].textContent = 'Colombia';
-  }
+for (let i = 0; i < countries.length; i++) {
+  const flagDiv = document.createElement('div');
+  const imageDiv = document.createElement('div');
+  const textDiv = document.createElement('div');
+  const p = document.createElement('p');
+
+  container.appendChild(flagDiv);
+  flagDiv.classList.add('flag-container');
+  flagDiv.appendChild(imageDiv);
+  imageDiv.classList.add('image');
+  flagDiv.appendChild(textDiv);
+  textDiv.classList.add('text-div');
+  textDiv.appendChild(p);
+  p.classList.add('text');
+
+  console.log(p.parentNode);
+
+}
+
+container.addEventListener('mouseover', () => {
+
 });
 
-container.addEventListener('mouseout', (e) => {
-  if (e.target === nigeria.children[0]) {
-    nigeria.children[1].children[0].textContent = '';
-  }  else if (e.target === us.children[0])  {
-    us.children[1].children[0].textContent = '';
-  }  else if (e.target === colombia.children[0])  {
-    colombia.children[1].children[0].textContent = '';
-  }
 
-});
+
+
+
+// container.addEventListener('mouseover', (e) => {
+//   if (e.target === nigeria.children[0]) {
+//     nigeria.children[1].children[0].textContent = 'Nigeria';
+//   }  else if (e.target === us.children[0])  {
+//     us.children[1].children[0].textContent = 'United States';
+//   }  else if (e.target === colombia.children[0])  {
+//     colombia.children[1].children[0].textContent = 'Colombia';
+//   }
+// });
+//
+// container.addEventListener('mouseout', (e) => {
+//   if (e.target === nigeria.children[0]) {
+//     nigeria.children[1].children[0].textContent = '';
+//   }  else if (e.target === us.children[0])  {
+//     us.children[1].children[0].textContent = '';
+//   }  else if (e.target === colombia.children[0])  {
+//     colombia.children[1].children[0].textContent = '';
+//   }
+// });
